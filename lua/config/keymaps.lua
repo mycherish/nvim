48557eccbc46ec4ebfb308313aa6e922a415d19b
xuweidong <determined_xw@126.com>
2025-08-13 15:57:18 +0800

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "跳转到定义" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "查找引用" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "显示文档" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "代码操作" })
+vim.keymap.set("n", "gt", ":tabnext", { desc = "Next Tab" })
+vim.keymap.set("n", "gT", ":tabprevious", { desc = "Previous Tab" })
